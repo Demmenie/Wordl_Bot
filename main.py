7c0d4a64ced09b54948bff82a4bd7148d5531d12
CDemmenie <11903978+Demmenie@users.noreply.github.com>
2022-01-16 00:06:14 +0000

    First working version.

--- main.py
+++ main.py
@@ -2,38 +2,74 @@
 #Chico Demmenie
 #Wordl_Bot/main.py
 
+#Getting dependencies
 import tweepy
 import csv
 import json
 import time
+import re
 from datetime import datetime
+
 
 class main:
 
     """Main class and loop of the bot"""
+
 
     #---------------------------------------------------------------------------
     def __init__(self):
 
         """Initialising the functions of the loop and getting auth."""
 
+        print("Initialising...")
         #Retrieving OAuth Twitter keys.
         self.keys = json.load(open("keys.json", "r"))
-        self.DoneReplies = []
 
-        #Requesting an auth from twitter so we can use their API.
-        self.api = tweepy.Client(bearer_token=self.keys["bearerToken"],
-            consumer_key=self.keys["apiKey"],
-            consumer_secret=self.keys["apiSecret"],
-            access_token=self.keys["accessToken"],
-            access_token_secret=self.keys["accessSecret"],
-            wait_on_rate_limit=True)
+        #Setting variables we need beforehand.
+        self.doneReplies = []
+        self.peopleWhoGuessed = []
+        self.day = self.keys["thisDay"]
+        self.authTime = None
+        self.thisTweetTime = time.time() - 80000
 
-        self.thisTweetTime = time.time() - 3600
-        self.replyList = self.getReplies()
+        self.mainLoop()
 
-        self.analyseReplies()
-        self.postReplies()
+
+    #---------------------------------------------------------------------------
+    def mainLoop(self):
+
+        """The main loop of the bot that calls functions."""
+
+        print("Entering main loop...")
+        while True:
+            self.wordlAnswer = False
+            self.makeWordl
+
+            #if (time.time() - 86400) > self.thisTweetTime:
+            self.makeWordl()
+            while self.wordlAnswer == False:
+                #Checking if we need a new authentication and requesting.
+                if (self.authTime == None or
+                    (self.authTime + 7100) < time.time()):
+
+                    self.api = tweepy.Client(
+                        bearer_token=self.keys["bearerToken"],
+                        consumer_key=self.keys["apiKey"],
+                        consumer_secret=self.keys["apiSecret"],
+                        access_token=self.keys["accessToken"],
+                        access_token_secret=self.keys["accessSecret"],
+                        wait_on_rate_limit=True
+                        )
+
+                #Calling the necessary functions
+                self.replyList = self.getReplies()
+                self.analyseReplies()
+
+                #if (time.time() - 86400) > self.thisTweetTime:
+                    #self.reset()
+                    #break
+
+                time.sleep(6)
 
 
     #---------------------------------------------------------------------------
@@ -41,16 +77,26 @@ class main:
 
         """A function that gets the next wordl and posts it."""
 
-        wordls = open("wordls.csv", "r")
-        wordls = csv.reader(wordls, delimiter=',')
+        #Grabbing the csv of wordls
+        print("Making Wordl...")
+        file = open("wordls.csv", "r")
+        wordls = csv.reader(file)
 
-        self.thisWordl = wordls[self.day]
-        tweet = ''.join(("Wordl ", self.day, "\N{black large square}"+
-            "\N{black large square}\N{black large square}\N{black large square}"
-            +"\N{black large square}"))
+        #Finding today's Wordl
+        for index, line in enumerate(wordls):
+            if index == self.day:
 
-        self.api.create_tweet(text=tweet)
-        self.thisTweetTime = int(time.time())
+                self.thisWordl = line
+                break
+
+        #Creating the day's tweet
+        tweet = ''.join(("Wordl ", str(self.day),
+            "\n\U00002B1B\U00002B1B\U00002B1B\U00002B1B\U00002B1B"))
+
+        #self.api.create_tweet(text=tweet)
+        #self.thisTweetTime = int(time.time())
+
+        print(f"Wordl {self.day}: {self.thisWordl[0]} - time: {time.time()}")
 
 
     #---------------------------------------------------------------------------
@@ -58,14 +104,18 @@ class main:
 
         """A function that gets all the unread replies to the latest wordl."""
 
-        latestTweets = self.api.get_users_tweets(id = self.keys["userID"],
-            max_results = 5)[0]
-        self.thisTweetID = latestTweets[0].id
+        try:
+            print("Getting replies...")
+            replies = self.api.search_recent_tweets(query="to:BotWordl",
+                max_results=100,
+                #start_time=datetime.fromtimestamp(self.thisTweetTime),
+                expansions='author_id')
 
-        replies = self.api.search_recent_tweets(query="to:BotWordl",
-            max_results=100,
-            start_time=datetime.fromtimestamp(self.thisTweetTime))
+        except Exception as e:
+            print(e)
 
+
+        print("Replies:", replies.data)
         return replies.data
 
 
@@ -77,32 +127,100 @@ class main:
 
         for reply in self.replyList:
 
+            #Creating variables needed during the logic
+            message = []
             replySplit = reply.text.split(" ")
-            if replySplit[0] == "@BotWordl" and (len(replySplit == 2) or
-                len(replySplit == 3):
 
-                if len(replySplit[-1]) != 5:
-                    message = "Sorry, that word is the wrong length."
+            #Making sure that the message was in reply to the bot
+            if (replySplit[0] == "@BotWordl" and
+                (len(replySplit) == 2 or len(replySplit) == 3)):
+
+                if reply.id not in self.doneReplies:
+
+                    if reply.author_id not in self.peopleWhoGuessed:
+
+                        self.doneReplies.append(reply.id)
+
+                        #Making sure that the word is the right length
+                        if len(replySplit[-1]) != 5:
+                            message = "Sorry, that word is the wrong length."
+                            self.TweetReply(reply.id, message)
+
+                        #Finding out if the user answered the wordl correctly
+                        elif replySplit[-1] == self.thisWordl[0]:
+                            message = ''.join(("\U0001F7E9\U0001F7E9\U0001F7E9"+
+                                "\U0001F7E9\U0001F7E9"+
+                                f"\n{self.thisWordl[0]} - {self.thisWordl[2]}"))
+
+                            self.TweetReply(reply.id, message)
+                            self.retweetReply(reply.id)
+                            self.wordlAnswer = True
+                            self.reset()
+                            break
+
+                        #If none of the above are true then we work out what the user
+                        #got rght and wrong.
+                        else:
+                            self.peopleWhoGuessed.append(reply.author_id)
+                            thisGuess = list(replySplit[-1])
+                            wordlList = list(self.thisWordl[0].upper())
+
+                            #A for loop that checks each letter of the answer.
+                            for index, letter in enumerate(thisGuess):
+
+                                if letter.upper() == wordlList[index]:
+                                    message.append(("\U0001F7E9"))
+
+                                elif letter.upper() in wordlList:
+                                    message.append(("\U0001F7E8"))
+
+                                else:
+                                    message.append(("\U00002B1B"))
+
+                    else:
+                        message = "You may only have one guess per Wordl."
+
+                    message = ''.join(message)
                     self.TweetReply(reply.id, message)
-
-                elif replySplit[-1] == self.thisWordl[0]:
-                    message = ''.join("\N{green square}\N{green square}"+
-                        "\N{green square}\N{green square}\N{green square} "+
-                        f"\n{self.thisWordl[0]} - {self.thisWordl[2]}"
-
-                    self.TweetReply(reply.id, message)
-
-                else:
-                    thisGuess = list(replySplit[-1])
-
 
 
     #---------------------------------------------------------------------------
     def TweetReply(self, replyID, message):
 
-        """A function that replies to tweets"""
+        """A function that replies to tweets."""
+        print("Replying:", message, "to:", replyID)
 
-        self.api.create_tweet(text=message, in_reply_to_tweet_id=replyID)
+        try:
+            self.api.create_tweet(text=message, in_reply_to_tweet_id=replyID)
+
+        except Exception as e:
+            print(f"error: {e}")
+
+
+    #---------------------------------------------------------------------------
+    def RetweetReply(self, replyID):
+
+        """A function that retweets any tweet based on a given id."""
+
+        try:
+            self.api.retweet(tweet_id=replyID)
+
+        except Exception as e:
+            print(e)
+
+
+    #---------------------------------------------------------------------------
+    def reset(self):
+
+        """Resets variables and gets ready for the next wordl."""
+
+        self.day = self.day + 1
+        self.keys["thisDay"] = self.day
+        open("keys.json", "w").write(json.dumps(self.keys))
+        self.peopleWhoGuessed = []
+        self.doneReplies = []
+        self.replyList
+
 
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
